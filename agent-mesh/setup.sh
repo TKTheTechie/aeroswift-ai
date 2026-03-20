10e9441ac64aedf127f5119e6152543fa22fca5b
LeahRobert <robert.leah11@gmail.com>
2026-03-20 14:09:12 -0400

    Setup.sh fix pathing issue

--- agent-mesh/setup.sh
+++ agent-mesh/setup.sh
@@ -17,6 +17,6 @@ cp agents/* configs/agents/
 cp gateways/* configs/gateways/
 
 # Create and pre-load the PostgreSQL databases; Frequent Flyer and Flight Information
-docker run -d --name aeroswift_ffmembers -p 5431:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=aeroswift_ffmembers -v ./data/ff_data:/data -v ./pgdata/ff_data.sql:/docker-entrypoint-initdb.d/init.sql postgres:14.22-trixie
-docker run -d --name aeroswift_flightdata -p 5433:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=aeroswift_flightdata -v ./data/flight_data:/data -v ./pgdata/flight_data.sql:/docker-entrypoint-initdb.d/init.sql postgres:14.22-trixie
+docker run -d --name aeroswift_ffmembers -p 5431:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=aeroswift_ffmembers -v ./data/ff_data:/data -v ./postgresql/ff_data.sql:/docker-entrypoint-initdb.d/init.sql postgres:latest
+docker run -d --name aeroswift_flightdata -p 5433:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=aeroswift_flightdata -v ./data/flight_data:/data -v ./postgresql/flight_data.sql:/docker-entrypoint-initdb.d/init.sql postgres:latest
 
